8e410df7cabf845811f198b8539a9ef1af496908
Victor <victorrb1015@gmail.com>
2020-08-31 17:11:31 -0500

    Curso

---
+++ src/App.svelte
@@ -0,0 +1,49 @@
+<script>
+	import About from "./components/About.svelte";
+	export let name;
+	export let lastName;
+	let svelteLogo = "https://arepa.s3.amazonaws.com/svelte-logo.png";
+
+</script>
+
+<main>
+	<h1>Hello {name} {lastName}!</h1>
+	<About />
+	<img src={svelteLogo} alt="Svelte" />
+</main>
+
+<style>
+	:global(body) {
+		background-color: #f2eee2;
+		color: #0084f6;
+	}
+
+	:global(:root) {
+		--theme-color: purple;
+		font-size: 22px;
+	}
+
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	main img {
+		width: 30%;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/components/About.svelte
@@ -0,0 +1,15 @@
+<script>
+let someText = 'Fullstack developer of Esquitechs';
+</script>
+
+<style>
+p{
+    color: var(--theme-color);
+}
+</style>
+
+<div class="About">
+<p>
+    {someText}
+</p>
+</div>
